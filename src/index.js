12354a6e3eb979363ec0b4a764af33a5246bce75
Satyam Gupta <2019335@iiitdmj.ac.in>
2021-07-01 07:39:14 +0530

    General Layout Drafted

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));  //App is a component,it is the location
+ReactDOM.render(<App />, document.getElementById('root'));
